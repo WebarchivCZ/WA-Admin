9a47cee8b5a5f4f454eb3390bebae70d75817514
Adam Brokes <adam.brokes@gmail.com>
2008-08-10 20:52:41 +0000

    repair # selected item in top menu # images in css

--- application/views/layout/top_nav.php
+++ application/views/layout/top_nav.php
@@ -5,15 +5,15 @@ $menu_items = Kohana::config('wadmin.top_menu');
 foreach ($menu_items as $item)
 {
 	$item_title = Kohana::lang('menu.'.$item);
+	$class = ucfirst(inflector::singular($item));
 	
-	if ($item == url::current())
+	if ($class == URI::segment(3))
 	{
 		echo "<li class='active'><span><span>$item_title</span></span></li>";
 	}
 	else
 	{
 		$base = url::base();
-		$class = ucfirst(inflector::singular($item));
 		$controller = $base.'table/view/'.$class;
 		echo '<li><span><span><a href="'.$controller.'">'.$item_title.'</a></span></span></li>';
 	}
