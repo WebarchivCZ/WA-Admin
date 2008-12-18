fdd49cfb97b36ccc829023e7ffe7f01c655917b7
Adam Brokes <adam.brokes@gmail.com>
2008-12-18 13:33:33 +0000

    uprava pohledu na vlozeni zdroje

--- application/views/insert_resource.php
+++ application/views/insert_resource.php
@@ -1,10 +1,15 @@
-<script src="http://dev.jquery.com/view/tags/ui/latest/ui/ui.datepicker.js"></script>
-<script type="text/javascript">
-<!--
-$('#date').datepicker();
-//-->
-</script>
-
 <h2>Vložení nového zdroje</h2>
 
-<?= $content ?>
+<?php
+if (isset($values)) {
+	$content = '<h2>Zdroj byl uspesne vlozen</h2>
+					<table>';
+	foreach ($values as $key => $value) {
+		$content .= "<tr><td>" . Kohana::lang('resource.'.$key)."</td><td>$value</td></tr>";
+	}
+	$content .= '</table>';
+}
+
+echo $content;
+
+?>
