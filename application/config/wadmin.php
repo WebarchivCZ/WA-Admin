dd40400997d9a7775799534431bdbe2840af7210
Adam Brokes <adam.brokes@gmail.com>
2009-11-02 11:14:39 +0000

    Deployment verze 2.0

--- application/config/wadmin.php
+++ application/config/wadmin.php
@@ -37,7 +37,7 @@ $config = array(
  */
 $config['debug_mode'] = TRUE;
 
-$config['version'] = '2.13';
+$config['version'] = '2.0';
 
-$config['ticket_url'] = 'http://raptor.webarchiv.cz:8000/trac/newticket?component=wa_admin&amp;milestone=WA Admin v2.02&amp;owner=brokes';
+$config['ticket_url'] = 'http://raptor.webarchiv.cz:8000/trac/newticket?component=wa_admin&amp;milestone=WA Admin v2.2&amp;owner=brokes';
 ?>
