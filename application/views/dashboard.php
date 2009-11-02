cddeb3aa9eaa0a3d6a2734fa292c061fe04e72ea
Adam Brokes <adam.brokes@gmail.com>
2009-11-02 00:07:42 +0000

    Drobne upravy dashboardu

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -1,10 +1,10 @@
-<div class="dashboard">Ahoj, máš:
+<div class="dashboard">Ahoj, máš
 <ul>
-	<li><a href='<?=url::base()?>rate'>Zdrojů k hodnocení: <?=$dashboard->to_rate ?></a></li>
-	<li><a href='<?=url::base()?>rate'>Ohodnocené zdroje: <?=$dashboard->new_rated ?></a></li>
-	<li><a href='<?=url::base()?>rate'>Zdroje k přehodnocení: <?=$dashboard->re_rate ?></a></li>
-	<li><a href='<?=url::base()?>catalogue'>Zdroje ke katalogizaci: <?=$dashboard->to_catalogue ?></a></li>
-	<li><a href='<?=url::base()?>addressing'>Zdroje k oslovení: <?=$dashboard->to_address ?></a></li>
+	<li><a href='<?=url::base()?>rate'>zdrojů k hodnocení: <?=$dashboard->to_rate ?></a></li>
+	<li><a href='<?=url::base()?>rate'>ohodnocené zdroje: <?=$dashboard->new_rated ?></a></li>
+	<li><a href='<?=url::base()?>rate'>zdroje k přehodnocení: <?=$dashboard->re_rate ?></a></li>
+	<li><a href='<?=url::base()?>catalogue'>zdroje ke katalogizaci: <?=$dashboard->to_catalogue ?></a></li>
+	<li><a href='<?=url::base()?>addressing'>zdroje k oslovení: <?=$dashboard->to_address ?></a></li>
 	<!--<li><a href='<?=url::base()?>addressing'>Zdroj bez odezvy: <?=$dashboard->no_response ?></a></li>-->
 </ul>
 </div>
