2f5eb8e7e1ca0ad1f33fb292f7a6f3460ad1e26f
Adam Brokes <adam.brokes@gmail.com>
2010-05-11 08:41:24 +0000

    PHP 5.3 - remove split() -> explode()

--- application/views/tables/record_qa_view.php
+++ application/views/tables/record_qa_view.php
@@ -1,55 +1,64 @@
-<?php if (isset($values)): ?>
+<?php
+if (isset ( $values )) {
+    ?>
 
-<?php echo table::header(); ?>
-        <tr>
-            <th class="first" width="60%">Sloupec</th>
-            <th class="last">Hodnota</th>
-        </tr>
-            <?php foreach ($values as $key => $value):
-                if ($key == 'url') {
-                    $value = "<a href='{$value}' target='_blank'>{$value}</a>";
-                }
-                if ($key == 'email') {
-                    $value = "<a href='mailto:{$value}'>{$value}</a>";
-                }
-                if ($key == 'result') {
-                    $value = Qa_Check_Model::get_result_value($value);
-                }
-                ?>
-        <tr>
-            <td><?= ucfirst(Kohana::lang('tables.'.$key)) ?></td>
-            <td><?= $value ?></td>
-        </tr>
-    <?php
+<?php
+    echo table::header ();
+    ?>
+<tr>
+	<th class="first" width="60%">Sloupec</th>
+	<th class="last">Hodnota</th>
+</tr>
+<?php
+    foreach ( $values as $key => $value ) {
+        if ($key == 'url') {
+            $value = "<a href='{$value}' target='_blank'>{$value}</a>";
+        }
+        if ($key == 'email') {
+            $value = "<a href='mailto:{$value}'>{$value}</a>";
+        }
+        if ($key == 'result') {
+            $value = Qa_Check_Model::get_result_value ( $value );
+        }
+        ?>
+<tr>
+	<td><?=ucfirst ( Kohana::lang ( 'tables.' . $key ) )?></td>
+	<td><?=$value?></td>
+</tr>
+<?php
     
-    endforeach;
-            
-	echo table::footer(); 
+    }
+    
+    echo table::footer ();
+    
+    echo "<h3>Problémy</h3>";
+    
+    $problems = $qa_check->qa_check_problems;
+    
+    foreach ( $problems as $problem ) {
+        
+        $urls = explode ( ' ', $problem->url );
+        
+        echo "<div class='problem'>";
+        echo "<p><strong>{$problem->qa_problem->question}?</strong></p>";
+        echo "<p>{$problem->comments}</p>";
+        if (count ( $urls ) > 0) {
+            echo "<ul>";
+            foreach ( $urls as $url ) {
+                if (! empty ( $url ))
+                    echo "<li>" . html::anchor ( $url, $url, array ('target' => '_blank' ) ) . "</li>";
+            }
+            echo "</ul>";
+        }
+        echo "</div>";
+    }
 
-	echo "<h3>Problémy</h3>";
-	
-	$problems = $qa_check->qa_check_problems;
-	
-	foreach ($problems as $problem) {
-		
-		$urls = split(' ', $problem->url);
-		
-		echo "<div class='problem'>";
-		echo "<p><strong>{$problem->qa_problem->question}?</strong></p>";
-		echo "<p>{$problem->comments}</p>";
-		if (count($urls) > 0) {
-    		echo "<ul>";
-    		foreach ($urls as $url){
-    			if (! empty($url))
-    			echo "<li>". html::anchor($url, $url, array('target'=>'_blank')) ."</li>";
-    		}
-    		echo "</ul>";
-		}
-		echo "</div>";
-	}
-	
-
-endif; ?>
-
+}
+?>
 <p>
-    <button onclick="history.back()" class="floatright">Zpět</button></p>
+<a href="<?= url::site('quality_control/edit/'.$qa_check->id) ?>"><button>Editace záznamu</button></a>
+<button onclick="history.back()" class="floatright">Zpět</button>
+</p>
+<p>
+<a href="<?= url::site('quality_control/add_solution/'.$qa_check->id) ?>"><button>Přidat řešení</button></a>
+</p>
