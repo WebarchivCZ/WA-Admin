8272706e413bc3da31e19a10c6734341f5e5382f
Adam Brokes <adam.brokes@gmail.com>
2010-10-14 14:35:20 +0000

    Upraven Dashboard.

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -1,14 +1,14 @@
 <div class="dashboard">Ahoj, máš
 <ul>
-	<li><a href='<?=url::base()?>rate#tabs-1'>zdroje k hodnocení: <?=$dashboard->to_rate ?></a></li>
-	<li><a href='<?=url::base()?>rate#tabs-2'>zdroje k přehodnocení: <?=$dashboard->re_rate ?></a></li>
-	<li><a href='<?=url::base()?>rate#tabs-3'>ohodnocené zdroje: <?=$dashboard->new_rated ?></a></li>
-	<li><a href='<?=url::base()?>rate#tabs-4'>přehodnocené zdroje: <?=$dashboard->new_rerated ?></a></li>
-	<li><a href='<?=url::base()?>addressing'>zdroje k oslovení: <?=$dashboard->to_address ?></a></li>
-	<li><a href='<?=url::base()?>progress'>zdroje v jednání: <?=$dashboard->in_progress ?></a></li>
-	<li><a href='<?=url::base()?>catalogue'>zdroje ke katalogizaci: <?=$dashboard->to_catalogue ?></a></li>
-	<li><a href='<?=url::base()?>quality_control'>zdroje k QA: <?=$dashboard->to_qa ?></a></li>
-	<li><a href='<?=url::base()?>conspectus'>nominované zdroje: <?=$dashboard->nominated ?></a></li>
+	<li><a href='<?=url::base()?>rate#tabs-1'><?=$dashboard->to_rate ?> - zdroje k hodnocení</a></li>
+	<li><a href='<?=url::base()?>rate#tabs-2'><?=$dashboard->re_rate ?> - zdroje k přehodnocení</a></li>
+	<li><a href='<?=url::base()?>rate#tabs-3'><?=$dashboard->new_rated ?> - ohodnocené zdroje</a></li>
+	<li><a href='<?=url::base()?>rate#tabs-4'><?=$dashboard->new_rerated ?> - přehodnocené zdroje</a></li>
+	<li><a href='<?=url::base()?>addressing'><?=$dashboard->to_address ?> - zdroje k oslovení</a></li>
+	<li><a href='<?=url::base()?>progress'><?=$dashboard->in_progress ?> - zdroje v jednání</a></li>
+	<li><a href='<?=url::base()?>catalogue'><?=$dashboard->to_catalogue ?> - zdroje ke katalogizaci</a></li>
+	<li><a href='<?=url::base()?>quality_control'><?=$dashboard->to_qa ?> - zdroje k QA</a></li>
+	<li><a href='<?=url::base()?>conspectus'><?=$dashboard->nominated ?> - nominované zdroje</a></li>
 	
 </ul>
 </div>
