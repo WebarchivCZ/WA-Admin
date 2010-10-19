ef1bd6d36623cb7b6d6d9462366f46f5b9bf8234
Adam Brokes <adam.brokes@gmail.com>
2010-10-19 08:10:16 +0000

    Opraveno poradi v dashboardu.

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -1,22 +1,37 @@
 <div class="dashboard">Ahoj, máš:
 <table>
 	<tr>
-		<td><a href='<?=url::base()?>rate#tabs-1'>zdroje k hodnocení</a></td>
-		<td><a href='<?=url::base()?>rate#tabs-1'><?=$dashboard->to_rate?></a></td>
+		<td><a href='<?=url::base()?>addressing'>zdroje k oslovení</a></td>
+		<td><a href='<?=url::base()?>addressing'><?=$dashboard->to_address?></a></td>
 	
 	</tr>
 	<tr>
-		<td><a href='<?=url::base()?>rate#tabs-2'>zdroje k přehodnocení</a></td>
-		<td><a href='<?=url::base()?>rate#tabs-2'><?=$dashboard->re_rate?></a></td>
+		<td><a href='<?=url::base()?>catalogue'>zdroje ke katalogizaci</a></td>
+		<td><a href='<?=url::base()?>catalogue'><?=$dashboard->to_catalogue?></a></td>
 	
 	</tr>
 	<tr>
-		<td><a href='<?=url::base()?>rate#tabs-3'>ohodnocené zdroje</a></td>
-		<td><a href='<?=url::base()?>rate#tabs-3'><?=$dashboard->new_rated?></a></td>
+		<td><a href='<?=url::base()?>quality_control'>zdroje k QA</a></td>
+		<td><a href='<?=url::base()?>quality_control'><?=$dashboard->to_qa?></a></td>
 	
 	</tr>
 	<tr>
 		<td colspan="2" class="show-all"><a href='#' id='show-all'>zobrazit vše</a></td>
+	
+	</tr>
+	<tr class="hidden">
+		<td><a href='<?=url::base()?>rate#tabs-1'>zdroje k hodnocení</a></td>
+		<td><a href='<?=url::base()?>rate#tabs-1'><?=$dashboard->to_rate?></a></td>
+	
+	</tr>
+	<tr class="hidden">
+		<td><a href='<?=url::base()?>rate#tabs-2'>zdroje k přehodnocení</a></td>
+		<td><a href='<?=url::base()?>rate#tabs-2'><?=$dashboard->re_rate?></a></td>
+	
+	</tr>
+	<tr class="hidden">
+		<td><a href='<?=url::base()?>rate#tabs-3'>ohodnocené zdroje</a></td>
+		<td><a href='<?=url::base()?>rate#tabs-3'><?=$dashboard->new_rated?></a></td>
 	
 	</tr>
 	<tr class="hidden">
@@ -25,23 +40,8 @@
 	
 	</tr>
 	<tr class="hidden">
-		<td><a href='<?=url::base()?>addressing'>zdroje k oslovení</a></td>
-		<td><a href='<?=url::base()?>addressing'><?=$dashboard->to_address?></a></td>
-	
-	</tr>
-	<tr class="hidden">
 		<td><a href='<?=url::base()?>progress'>zdroje v jednání</a></td>
 		<td><a href='<?=url::base()?>progress'><?=$dashboard->in_progress?></a></td>
-	
-	</tr>
-	<tr class="hidden">
-		<td><a href='<?=url::base()?>catalogue'>zdroje ke katalogizaci</a></td>
-		<td><a href='<?=url::base()?>catalogue'><?=$dashboard->to_catalogue?></a></td>
-	
-	</tr>
-	<tr class="hidden">
-		<td><a href='<?=url::base()?>quality_control'>zdroje k QA</a></td>
-		<td><a href='<?=url::base()?>quality_control'><?=$dashboard->to_qa?></a></td>
 	
 	</tr>
 	<tr class="hidden">
