499685e694abd2d9cac520c80e085e0176962f3c
Adam Brokes <adam.brokes@gmail.com>
2012-03-31 21:40:59 +0200

    cleanup & new constants for contract

--- application/config/constants.php
+++ application/config/constants.php
@@ -1,4 +1,5 @@
 <?php
+// Resource Statuses
 define('RS_NEW', 1);
 define('RS_APPROVED_WA', 2);
 define('RS_RE_EVALUATE', 3);
@@ -8,4 +9,10 @@ define('RS_REJECTED_PUB', 6);
 define('RS_NO_RESPONSE', 7);
 define('RS_CONTACTED', 8);
 define('RS_END_PUBLISHING', 9);
+
+// Contract Types
+define('CT_NORMAL', 'NORMAL');
+define('CT_ADDENDUM', 'ADDENDUM');
+define('CT_BLANCO', 'BLANCO');
+
 ?>
