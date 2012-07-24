3636bd33e79d8832c3eef15d916e6903aad0aaef
Adam Brokes <adam.brokes@gmail.com>
2012-07-24 23:04:44 +0200

    fix for contract view (swap elements and identify blanco more clearly)

--- application/views/tables/append_contract.php
+++ application/views/tables/append_contract.php
@@ -2,15 +2,19 @@
 if (!isset($contract)) {
     throw new WaAdmin_Exception('Contract ID is not set', 'ID of viewied contract is not set.');
 }
-$addendums = $contract->get_addendums();
-if ($addendums->count() > 0) {
-    echo '<h3 class="record-information">K této smlouvě přísluší tyto dodatky:</h3>';
-    foreach ($addendums as $addendum) {
-        View::factory('tables/contracts/list_resources')->bind('resources', $addendum->get_resources())->render(TRUE);
-    }
-}
-
 if (isset($resources) AND $resources->count() != 0) {
     echo '<h3 class="record-information">Zdroje</h3>';
     View::factory('tables/contracts/list_resources')->bind('resources', $resources)->render(TRUE);
+}
+$addendums = $contract->get_addendums();
+if ($addendums->count() > 0) {
+    if ($contract->is_blanco()) {
+        $addendum_message = 'K blanco smlouvě přísluší tyto smlouvy:';
+    } else {
+        $addendum_message = 'K této smlouvě přísluší tyto dodatky:';
+    }
+    echo "<h3 class='record-information'>{$addendum_message}</h3>";
+    foreach ($addendums as $addendum) {
+        View::factory('tables/contracts/list_resources')->bind('resources', $addendum->get_resources())->render(TRUE);
+    }
 }
