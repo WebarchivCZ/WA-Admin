c39e65c1618660b189bb09a34e78bd161b7d7221
Adam Brokes <adam.brokes@gmail.com>
2012-07-25 00:16:17 +0200

    add - hook to prevent modification production data through test environment

---
+++ application/hooks/secure_production.php
@@ -0,0 +1,12 @@
+<?php
+$is_debug_mode = Kohana::config("wadmin.debug_mode");
+
+$db_name = Kohana::config('database.default.connection.database');
+$production_db_name = Kohana::config('wadmin.production_db_name');
+
+if ($is_debug_mode AND ($db_name == $production_db_name)) {
+    $administrator_email = Kohana::config('wadmin.administrator_email');
+    throw new WaAdmin_Exception('Production DB is set on test environment',
+        "Production database is set on testing environment. Please contact administrator:
+        <a href='mailto:{$administrator_email}'>{$administrator_email}</a>");
+}
