bfe851fd52e3aadd62b5688adaa4682eccd23746
Adam Brokes <adam.brokes@gmail.com>
2012-10-09 22:35:15 +0200

    Add example

--- sql/1000-wadmin-version.sql
+++ sql/1000-wadmin-version.sql
@@ -1,4 +1,4 @@
 -- This file contains versioning info.
 -- Each line should look like:
--- Update
+-- update application_info set `value` = INT_VERSION_NUMBER, date_changed = now() where `key` = 'application_build';
 update application_info set `value` = 37, date_changed = now() where `key` = 'application_build';
