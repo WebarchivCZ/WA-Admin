ba4077858d92ee9d0e662c8b8a61e1178ea7199e
Adam Brokes <adam.brokes@gmail.com>
2012-10-09 22:33:02 +0200

    Replace double quotes in sql file.

--- sql/0007-wadmin-add-application_info.sql
+++ sql/0007-wadmin-add-application_info.sql
@@ -20,15 +20,15 @@ INSERT INTO
                 `date_created` ,
                 `date_changed`
         )
-        VALUES (''application_name'', ''WA Admin'', ''APP'', ''Name of application.'', NOW( ) , NOW( )),
-                (''application_version'', ''2'', ''APP'', ''Major version of application.'', NOW( ) , NOW( )),
-                (''application_build'', ''36'', ''APP'', ''Minor version of application. This is increased with new deployed changes.'', NOW( ) , NOW( )),
-                (''database_version'', ''1'', ''DB'', ''Version of database.'', NOW( ) , NOW( )),
-                (''database_production_db_name'', ''wadmin'', ''DB'', ''Name of production DB.'', NOW( ) , NOW( )),
-                (''database_test_db_name'', ''wadmin-test'', ''DB'', ''Name of test DB.'', NOW( ) , NOW( )),
-                (''application_debug_mode'', ''TRUE'', ''APP'', ''TRUE if deployed instance is running in debug mode.'', NOW( ) , NOW( ));
+        VALUES ('application_name', 'WA Admin', 'APP', 'Name of application.', NOW( ) , NOW( )),
+                ('application_version', '2', 'APP', 'Major version of application.', NOW( ) , NOW( )),
+                ('application_build', '36', 'APP', 'Minor version of application. This is increased with new deployed changes.', NOW( ) , NOW( )),
+                ('database_version', '1', 'DB', 'Version of database.', NOW( ) , NOW( )),
+                ('database_production_db_name', 'wadmin', 'DB', 'Name of production DB.', NOW( ) , NOW( )),
+                ('database_test_db_name', 'wadmin-test', 'DB', 'Name of test DB.', NOW( ) , NOW( )),
+                ('application_debug_mode', 'TRUE', 'APP', 'TRUE if deployed instance is running in debug mode.', NOW( ) , NOW( ));
 
 -- Update database version info
 UPDATE `application_info`
-  SET `value` = ''7'', `date_changed` = NOW( )
-  WHERE `key` = ''database_version'';
+  SET `value` = '7', `date_changed` = NOW( )
+  WHERE `key` = 'database_version';
