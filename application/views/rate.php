d85e6031e78b8ea9bc46685b0d26b2e45851cb99
Adam Brokes <adam.brokes@gmail.com>
2012-10-21 11:30:46 +0200

    Fix problem for Rating Controller when curator doesn't have any resources for rate.

--- application/views/rate.php
+++ application/views/rate.php
@@ -10,39 +10,20 @@
             (<?= is_null($rated_resources_reevaluate) ? "0" : $rated_resources_reevaluate->count() ?>)</a></li>
     </ul>
 	<?php
-	if (isset($resources_new) and $resources_new)
+
+	$resources = array(1=> $resources_new,
+					   2=> $resources_reevaluate,
+					   3=> $rated_resources_new,
+					   4=> $rated_resources_reevaluate);
+
+	foreach ($resources as $tab_id => $resource_set)
 	{
+		$status = ($tab_id % 2 == 0) ? RS_RE_EVALUATE : RS_NEW;
+
 		$view = View::factory('forms/ratings_final');
 		$view->status = RS_NEW;
-		$view->resources = $resources_new;
-		$view->tab_id = 1;
-		$view->render(TRUE);
-
-	}
-	if (isset($resources_reevaluate) and $resources_reevaluate)
-	{
-		$view = View::factory('forms/ratings_final');
-		$view->status = RS_RE_EVALUATE;
-		$view->resources = $resources_reevaluate;
-		$view->tab_id = 2;
-		$view->render(TRUE);
-
-	}
-	if (isset($rated_resources_new) and $rated_resources_new)
-	{
-		$view = View::factory('forms/ratings_final');
-		$view->status = RS_NEW;
-		$view->resources = $rated_resources_new;
-		$view->tab_id = 3;
-		$view->render(TRUE);
-	}
-
-	if (isset($rated_resources_reevaluate) and $rated_resources_reevaluate)
-	{
-		$view = View::factory('forms/ratings_final');
-		$view->status = RS_RE_EVALUATE;
-		$view->resources = $rated_resources_reevaluate;
-		$view->tab_id = 4;
+		$view->resources = $resource_set;
+		$view->tab_id = $tab_id;
 		$view->render(TRUE);
 	}
 	?>
